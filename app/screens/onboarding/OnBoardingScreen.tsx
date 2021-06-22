2c4b218e6c7a2e4471eb625535fcbde530fe3255
adasilvapdev <adasilvapdev@gmail.com>
2021-06-22 15:45:21 -0400

    Before implementing paginator

--- app/screens/onboarding/OnBoardingScreen.tsx
+++ app/screens/onboarding/OnBoardingScreen.tsx
@@ -1,19 +1,39 @@
-import React from 'react'
-import { StyleSheet, Text, Image, View, FlatList, useWindowDimensions } from 'react-native'
+import React, { useState, useRef } from 'react'
+import { StyleSheet, View, FlatList } from 'react-native'
+import Animated from 'react-native-reanimated'
 import { OnboardingItem } from '../../components/organisms'
 import { SLIDES } from '../../utils'
 
 const OnBoardingScreen: React.FC = () => {
+  const [currentIndex, setCurrentIndex] = useState(0)
+  const scrollX = useRef(new Animated.Value(0)).current
+  const slidesRef = useRef(null)
+
+  const viewableItemsChanged = useRef(({ viewableItems }: any) => {
+    setCurrentIndex(viewableItems[0].items)
+  }).current
+  const viewConfig = useRef({ viewAreaCoveragePercentThreshold: 50 }).current
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={SLIDES}
-        renderItem={({ item }) => <OnboardingItem item={item} />}
-        horizontal
-        showsHorizontalScrollIndicator
-        pagingEnabled
-      />
+      <View style={{ flex: 3 }}>
+        <FlatList
+          data={SLIDES}
+          keyExtractor={(item) => item.id}
+          renderItem={({ item }) => <OnboardingItem item={item} />}
+          horizontal
+          showsHorizontalScrollIndicator
+          pagingEnabled
+          bounces={false}
+          onScroll={Animated.event([{ naviteEvent: { contentOffset: { x: scrollX } } }], {
+            useNativeDriver: false
+          })}
+          scrollEventThrottle={32}
+          onViewableItemsChanged={viewableItemsChanged}
+          viewabilityConfig={viewConfig}
+          ref={slidesRef}
+        />
+      </View>
     </View>
   )
 }
